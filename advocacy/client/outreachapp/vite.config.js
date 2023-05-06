c49397a8a58f22710c0a501083f9d304a042a715
Shawn <69310260+shawnsuzukidesign@users.noreply.github.com>
2023-05-06 14:23:52 -0700

    committing

--- advocacy/client/outreachapp/vite.config.js
+++
@@ -1,9 +0,0 @@
-// client/vite.config.js
-import { defineConfig } from 'vite'
-import { vitePlugin as swcPlugin } from '@swc/core'
-
-export default defineConfig({
-  plugins: [swcPlugin()],
-  base: import.meta.env.PROD ? '/client/' : '/'
-})
-
